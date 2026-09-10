5d3a042970d13879cf54fa94f68ba3c5f8ba353b
Moncayo1329 <moncayomichael23@gmail.com>
2026-09-10 22:52:35 +0100

    update with open

--- index.py
+++ index.py
@@ -1,17 +1,16 @@
 import random 
-import json
+import json 
+import webbrowser 
+
+with open("data.json") as data:
+    goles = json.load(data)
 
 
-data = open("data.json")
-goles = json.load(data) 
-
-
-
-gol_elegido = random.choice(goles)
-
-
+print("Bievenido a la app de goles del goat 🐐")
+print(f"Tenemos disponibles {len(goles)} goles del goat")
 
 while True: 
     gol_elegido = random.choice(goles)
-    print(f"Tu gol elegido de hoy:{gol_elegido['rival']} - {gol_elegido['competición']} - {gol_elegido['año']} - {gol_elegido['link']}")
+    print(f"Tu gol elegido de hoy:{gol_elegido['rival']} - {gol_elegido['competición']} - {gol_elegido['año']}")
     input("Presiona para sacar otro gol del goat")
+    webbrowser.open(gol_elegido["link"])
